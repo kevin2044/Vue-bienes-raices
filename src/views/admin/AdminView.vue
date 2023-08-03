89bac489a61ce82056c2383d6c13b2c59319f05b
Kevin Luna <kevin_2044@hotmail.com>
2023-08-02 23:28:49 -0600

    Agregando Formulario para crear una nueva propiedad

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -4,6 +4,13 @@
 
 <template>
     <h2 class="text-center text-h3 my-5 font-weight-bold">AdminView</h2>
+    <v-btn
+        color="blue"
+        variant="flat"
+        :to="{name: 'nueva-propiedad'}"
+    >
+        Nueva Propiedad
+    </v-btn>
 </template>
 
 <style scoped>
--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,9 +1,127 @@
 <script setup>
+import { useForm, useField } from 'vee-validate'
+import { useRouter } from 'vue-router'
+import { collection, addDoc } from 'firebase/firestore'
+import { useFirestore } from 'vuefire'
+import { validationSchema, imageSchema } from '@/validation/propiedadSchema'
 
+const items = [1,2,3,4,5]
+const db = useFirestore()
+const router = useRouter()
+const { handleSubmit } = useForm({
+    validationSchema: {
+        ...validationSchema,
+        ...imageSchema,
+    }
+})
+const titulo = useField('titulo')
+const imagen = useField('imagen')
+const precio = useField('precio')
+const habitaciones = useField('habitaciones')
+const wc = useField('wc')
+const estacionamiento = useField('estacionamiento')
+const descripcion = useField('descripcion')
+const alberca = useField('alberca', null, {
+    initialValue: false
+})
+
+const submit = handleSubmit(async(values) => {
+    const { imagen, ...propiedad} = values
+
+    console.log(propiedad)
+    const docRef = await addDoc(collection(db, "propiedades"), {
+        ...propiedad
+    });
+    if(docRef.id){
+        router.push({name: 'admin-propiedades'})
+    }
+})
 </script>
 
 <template>
     <h2 class="text-center text-h3 my-5 font-weight-bold">AdminNueva</h2>
+    <v-card
+        max-width="800"
+        flat
+        class="mx-auto"
+    >
+        <v-card-title
+            class="text-h3 font-weight-bold py-5"
+            tag="h3"
+        >
+            Nueva Propiedad
+        </v-card-title>
+        <v-card-subtitle
+            class="text-h5 py-5"
+        >
+            Crea una nueva propiedad llenando el siguiente formulario
+        </v-card-subtitle>
+        <v-form class="mt-10">
+            <v-text-field 
+                class="mb-5"
+                label="Titulo Propiedad"
+                v-model="titulo.value.value"
+                :error-messages="titulo.errorMessage.value"
+            />
+            <v-file-input 
+                accept="image/jpeg"
+                label="Fotografía"
+                prepend-icon="mdi-camera"
+                class="mb-5"
+                v-model="imagen.value.value"
+                :error-messages="imagen.errorMessage.value"
+            />
+            <v-text-field 
+                class="mb-5"
+                label="Precio"
+                v-model="precio.value.value"
+                :error-messages="precio.errorMessage.value"
+            />
+            <v-row>
+                <v-col cols="12" md="4">
+                    <v-select 
+                        label="Habitaciones"
+                        class="mb-5"
+                        :items="items"
+                        v-model="habitaciones.value.value"
+                        :error-messages="habitaciones.errorMessage.value"
+                    />
+                </v-col>
+                <v-col cols="12" md="4">
+                    <v-select 
+                        label="WC"
+                        class="mb-5"
+                        :items="items"
+                        v-model="wc.value.value"
+                        :error-messages="wc.errorMessage.value"
+                    />
+                </v-col>
+                <v-col cols="12" md="4">
+                    <v-select 
+                        label="Lugares Estacionamiento"
+                        class="mb-5"
+                        :items="items"
+                        v-model="estacionamiento.value.value"
+                        :error-messages="estacionamiento.errorMessage.value"
+                    />
+                </v-col>
+            </v-row>
+            <v-textarea 
+                class="mb-5" 
+                label="Descripcion" 
+                v-model="descripcion.value.value"
+                :error-messages="descripcion.errorMessage.value"
+            />
+            <v-checkbox label="Alberca" v-model="alberca.value.value" />
+            <v-btn
+                color="pink-accent-3"
+                block
+                @click="submit"
+            >
+                Agregar Propiedad
+            </v-btn>
+        </v-form>
+    </v-card>
 </template>
 
 <style scoped>
